82ac60f1345614f50660c61cbbd164239f9133d3
pannous <info@pannous.com>
2025-07-23 15:23:12 +0200

    printf undone

--- goo/test_printf.go
+++ goo/test_printf.go
@@ -1,7 +1,5 @@
 package main
 
 func main() {
-	print("Hello, ")  // writes to stderr
-	println("world!") // adds newline and spaces
-	printf("The answer is:", 42) // printf as builtin, works like println
+	printf("Hello world")
 }
