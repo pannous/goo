3256c40ccb3fb8f5e5a1521c6ba33f1f5251f2d6
pannous <info@pannous.com>
2025-12-11 17:36:44 +0100

    feat(minor): build bin/go in incremental compiler build
    - Added go command build to build-compiler.sh
    - Now rebuilds both compiler and go command incrementally
    - Ensures bin/go always has latest transforms and version

--- src/build-compiler.sh
+++ src/build-compiler.sh
@@ -6,5 +6,10 @@ GOCACHE=/tmp/go-cache
 GOOS=darwin
 GOARCH=arm64
 cd /opt/other/go/src/
+
+# Build compiler
 $GOROOT/bin/go build -tags=transforms -o ../bin/compile ./cmd/compile
 cp ../bin/compile ../pkg/tool/darwin_arm64/compile
+
+# Build go command
+$GOROOT/bin/go build -tags=transforms -o ../bin/go ./cmd/go
