d002ff7f60b5966c4d95109c03638b53b8c60fdb
pannous <info@pannous.com>
2025-12-11 18:51:25 +0100

    docs: clarify incremental build doesn't update version
    - Simplified build-compiler.sh to remove version update logic
    - Added clear documentation that incremental builds keep version
    - Full rebuild (make.bash) required for version updates
    - This is by design: incremental = fast dev, full = version tracking

--- src/build-compiler.sh
+++ src/build-compiler.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Incremental build script for development
+# Note: Does NOT update version string - use ./make.bash for version updates
+
 GOROOT=/opt/other/go
 GOTMPDIR=/tmp/go-debug
 GOROOT_FINAL=/opt/other/go
@@ -7,22 +11,7 @@ GOOS=darwin
 GOARCH=arm64
 cd /opt/other/go/src/
 
-# Update version in zbootstrap.go with current git hash
-HASH=$(git -C /opt/other/go rev-parse --short HEAD 2>/dev/null || echo "unknown")
-if [ "$HASH" != "unknown" ]; then
-  # Check if version changed
-  CURRENT_VERSION=$(grep "const version = " /opt/other/go/src/internal/buildcfg/zbootstrap.go | cut -d'`' -f2)
-  NEW_VERSION="go1.26.1.$HASH"
-
-  if [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then
-    echo "Updating version from $CURRENT_VERSION to $NEW_VERSION"
-    sed -i '' "s/const version = \`go1\.[0-9]*\.[0-9]*\.[a-f0-9]*\`/const version = \`$NEW_VERSION\`/" \
-      /opt/other/go/src/internal/buildcfg/zbootstrap.go
-    # Clear build caches when version changes
-    GOCACHE=/tmp/go-cache $GOROOT/bin/go clean -cache
-    $GOROOT/bin/go clean -cache  # Also clean default cache
-  fi
-fi
+echo "Incremental build (version will not update - use make.bash for full rebuild)"
 
 # Build compiler
 $GOROOT/bin/go build -tags=transforms -o ../bin/compile ./cmd/compile
@@ -30,3 +19,5 @@ cp ../bin/compile ../pkg/tool/darwin_arm64/compile
 
 # Build go command
 $GOROOT/bin/go build -tags=transforms -o ../bin/go ./cmd/go
+
+echo "Done! Run './bin/go version' to see current version"
