2a3bdfe284bd9092bbde5c9d6da87dbd11d6e1ac
pannous <info@pannous.com>
2025-12-11 19:15:47 +0100

    chore: update binaries and rebuild script

--- src/rebuild.sh
+++ src/rebuild.sh
@@ -1,1 +1,4 @@
+echo "full rebuild changes version string"
+cd src
+./make.bash
 GOROOT_BOOTSTRAP=/opt/other/go-darwin-arm64-bootstrap /opt/other/go-darwin-arm64-bootstrap/bin/go install -v cmd/compile
